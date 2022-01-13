fbf051d1a87386c1ed3665e8fc49413b81166cc3
Pichau <Pichau@DESKTOP-3KCIM55>
2022-01-13 20:26:29 -0300

    Criando variável com tipo int.

--- iniciandoJava/src/exercicios/TiposDeDados.java
+++ iniciandoJava/src/exercicios/TiposDeDados.java
@@ -2,5 +2,12 @@ package exercicios;
 
 public class TiposDeDados {
 
-	
+	public static void main(String[] args) {
+		
+		// Valor padrão sempre sará zero.
+		int numero;
+		
+		// Aqui a variável está sendo iniciada com o valor zero.
+		int numero2 = 0;
+	}
 }
